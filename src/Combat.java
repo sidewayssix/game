96564a2e9a33859ca4b0bb988eae3e59c6f295e0
sidewayssix <caldablog@gmail.com>
2019-12-13 14:25:08 -0500

    Update Combat.java

--- src/Combat.java
+++ src/Combat.java
@@ -26,37 +26,65 @@ public class Combat {
         while (true) {
             if (enemy.getHp() <= 0) {
                 System.out.println("You've won the Battle!");
+                player.setHp(player.getMaxHp);
                 break;
             }
 
             if (player.getHp() <= 0) {
                 System.out.println("You lost the battle, Game Over");
-                break;
+                System.out.println("-Restart- or" + " -Exit the Game-");
+				
+	            boolean validInput = false;
+                while (!validInput) {
+                   String userCommand = input.nextLine();
+                   if (userCommand.equals("Restart") {
+                        validInput = true;
+	                    game.main(); 
+                   }
+	               if (userCommand.equals("Exit the Game") {
+	                    System.exit();
+	               }
+                }
             }
 
-            System.out.println("What do you do?");
-            String userCommand = input.nextLine();
-            if (userCommand.equals("Attack")) {
-                if (value == 0) {
-                    damage = 0;
-                } else if (value >= 1 || value <= 4) {
-                    damage = weaponValue;
-                } else {
-                    damage = weaponValue * 2;
+            boolean validInput = false;
+            while (!validInput)
+            {
+                System.out.println("What do you do?");
+                String userCommand = input.nextLine();
+                if (userCommand.equals("Attack"))
+                {
+                    validInput = true;
+                    if (value == 0)
+                    {
+                        damage = 0;
+                    } else if (value >= 1 || value <= 4)
+                    {
+                        damage = weaponValue;
+                    } else
+                    {
+                        damage = weaponValue * 2;
+                    }
+                    
+                    int dmgDealt= weaponValue - enemy.getDef();
+                    System.out.println("You attack the " + enemy.getName() + " for " + dmgDealt " Damage.");
+                    enemy.setHp(weaponValue - enemy.getDef());
+                    
+                    int dmgReceived = enemy.getAtt() - shieldValue;
+                    System.out.println("The enemy attack you for " + dmgReceived + " Damage");
+                    player.setHp(enemy.getAtt() - shieldValue);
                 }
 
-                enemy.setHp(weaponValue - enemy.getDef());
-            }
-            
-            if (userCommand.equals("Throw Rock")) {
-                item.useRock();
-            }
-            
-            if (userCommand.equals("Use Potion")) {
-                item.usePotion();
-            }
-            else {
-                
-            }
+                if (userCommand.equals("Throw Rock"))
+                {
+                    validInput = true;
+                    item.useRock();
+                }
+
+                if (userCommand.equals("Use Potion"))
+                {
+                    validInput = true;
+                    item.usePotion();
+                }
         }
     }
